61ade5a44efac8b2741f15f0fdbd851e7db1137c
juiceghost <jg@renegade.se>
2020-09-29 10:34:25 +0200

    Tweaked styles, thanks J.R.

--- src/AppStyles.styles.tw.js
+++ src/AppStyles.styles.tw.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 
-const Styles = tw.div`w-full h-screen flex flex-col items-center justify-center`;
-const AppStyles = styled(Styles)`
+const AppStyles = styled.div`${tw`w-full h-screen flex flex-col items-center justify-center`};
   & {
     h1 {
       ${tw`font-sans text-6xl font-hairline text-6xl text-teal-500`}
