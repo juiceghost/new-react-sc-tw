eb90af878c792acaffa9f4b04c7ca6daf278c2a2
juiceghost <jg@renegade.se>
2020-09-29 10:25:53 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppStyles from "./AppStyles.styles.tw";
+import { GlobalStyles } from 'twin.macro'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <GlobalStyles />
+      <AppStyles>
+        <h1>Greetings Earthling</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Welcome to your Create-React-App / TailwindCSS / Styled Components
+          template
+      </p>
+        <h2>Resources / Documentation</h2>
+        <ul>
+          <li>
+            <a href="https://reactjs.org/docs/create-a-new-react-app.html">
+              ReactJS
+          </a>
+          </li>
+          <li>
+            <a href="https://tailwindcss.com/">TailwindCSS</a>
+          </li>
+          <li>
+            <a href="https://styled-components.com/">Styled Components</a>
+          </li>
+        </ul>
+      </AppStyles>
+    </>
   );
 }
 
